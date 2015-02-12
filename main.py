a07500b34f712b5e2d30163dbba8b5026edb33f1
10280588 <mennovleeuwen1990@gmail.com>
2015-02-12 21:47:24 +0100

    Based on given arguments the right step from the assignment is chosen, start and stop states not implemented yet. in main init function you can add the different cases

--- main.py
+++ main.py
@@ -12,13 +12,28 @@ class Main():
         self.n = 0
         self.m = 0
         self.corpusList = []
+        self.cp = None
+        self.sp = None
+        self.perm = False
+        self.case = ''
 
-        if step == 1:
-            self.argumentReader()
-            self.startPrinter()
+        self.argumentReader()
+        self.caseOptions()
+
+        if self.case == '1':
+            self.printer1()
             self.fileReaderStep1()
+            ngrams.Ngrams(self.corpusList, self.n, self.m)
+        elif self.case == '2.1':
+            self.printer1()
+        elif self.case == '2.2':
+            self.printer1()
+        elif self.case == '2.3':
+            self.printer1()
+        elif self.case == '2.4':
+            self.printer24()
 
-        ngrams.Ngrams(self.corpusList, self.n, self.m)
+
     #Takes care of provided arguments, if none given use default!
     def argumentReader(self):
         # Make a nice way to handle command line arguments
@@ -27,17 +42,51 @@ class Main():
         parser.add_argument('-corpus', required=True,
         help='Provide a text corpus file in the .txt format to perform an N-gram calculation.')
         # -n argument
-        parser.add_argument('-n', type=int, required=True,
+        parser.add_argument('-n',  type=int,
         help='Provide a number (integer) to calculate N-grams with word sequences of length n.')
         # -m argument
-        parser.add_argument('-m', type=int, required=True,
+        parser.add_argument('-m', type=int,
         help='Provide a number (integer) to display the m-th most frequent sequences.')
+        # -conditional-prob-file
+        parser.add_argument('-conditional-prob-file',
+        help='Provide a text corpus file in the .txt format to use for conditional probability')
+        # -sequence-prob-file argument
+        parser.add_argument('-sequence-prob-file',
+        help='Provide a text corpus file in the .txt format')
+        # -scored-permutations argument
+        parser.add_argument('-scored-permutations', action='store_true')
+
         args = parser.parse_args()
         self.corpus = args.corpus
         self.n = args.n
         self.m = args.m
+        self.cp = args.conditional_prob_file
+        self.sp = args.sequence_prob_file
+        self.perm = args.scored_permutations
+
+    #Decides what we are going to do, given the provided arguments.
+    def caseOptions(self):
+        print
+        print 'The following part of the assignment is performed:'
+        if  self.m > 0:
+            print 'Do step 1'
+            self.case = '1'
+        elif self.n > 0:
+            print 'Do step 2.1'
+        elif self.cp != None:
+            print 'Do step 2.2'
+            print self.cp
+        elif self.sp != None:
+            print 'Do step 2.3'
+        elif self.perm == True:
+            print 'Do step 2.4'
+        else:
+           print 'The program has found no options, nothing to do here.'
+           print 'Please read the documentation.'
+        print '-----------------------'
+
     #Prints an initial start message as well as the used values.
-    def startPrinter(self):
+    def printer1(self):
         print
         print 'The program to calculate N-grams of a corpus has started.'
         print 'You can find help about the working by typing main.py -h'
@@ -46,7 +95,14 @@ class Main():
         print 'The used corpusfile is: ' + self.corpus
         print 'We will calculate the N-Gram with sequences of length: ' + str(self.n)
         print 'The number of most frequent sequences (m) is: ' + str(self.m)
-        print 
+        print
+
+    def printer24(self):
+        print
+        print 'The program will now calculate permutations for you'
+        print 'The used corpusfile is: ' + self.corpus
+        print
+
     #Opens our corpus.txt file and converts it to a list of words.
     def fileReaderStep1(self):
         f = open(self.corpus, 'r')
@@ -54,5 +110,3 @@ class Main():
             for word in line.split():
                 self.corpusList.append(word)
         f.close()
-
-
