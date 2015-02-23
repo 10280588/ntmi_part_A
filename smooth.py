132a64f2cb5b80260371e59a88ee4a75562c56fc
maftrew <jelmer_alphenaar@hotmail.com>
2015-02-23 01:32:10 +0000

    calculateN enzo

--- smooth.py
+++ smooth.py
@@ -4,37 +4,33 @@ class Smooth():
     def add1(self, ngram):
         smoothedNgram = {key: value+1 for (key, value) in ngram.iteritems()}
         return smoothedNgram
-        
-    def GT(self, ngram, corpusLength):
+       
+    def calculateN(self, ngram):
         N = [0,0,0,0,0,0]
-        
         for key, value in ngram.iteritems():
             if value == 1:
-                N[1] = N[1] + 1
+                N[0] = N[0] + 1
             if value == 2:
-                N[2] = N[2] + 1
+                N[1] = N[1] + 1
             if value == 3:
-                N[3] = N[3] + 1
+                N[2] = N[2] + 1
             if value == 4:
-                N[4] = N[4] + 1
+                N[3] = N[3] + 1
             if value == 5:
-                N[5] = N[5] + 1
+                N[4] = N[4] + 1
             if value == 6:
-                N[6] == N[6] + 1
+                N[5] = N[5] + 1      
+        return N
+        
+    def GT(self, ngram, corpusLength, N):
         for key, value in ngram.iteritems():             # for every entry in the ngram
             newValue = self.GTSmoothing(ngram, value, N, corpusLength) # return the new value using GT formula 
             ngram.update({key:newValue})                 # and update entry to new value
-            return ngram
-            
-        #need for prob: len(yourdict.keys()) or len(yourdict) for vocabulary count
-        #N = number words total (or ngrams)
+        return ngram
 
     def GTSmoothing(self, ngram, value, N, corpusLength):
-        if value == 0:
-            return N[1]/(corpusLength)
-        else:
             if value <= 5:
-                newValue = ((value + 1) * N[value+1])/N[value]
+                newValue = ((value + 1) * N[value])/N[value-1]
                 return newValue
             else: 
                 return value
