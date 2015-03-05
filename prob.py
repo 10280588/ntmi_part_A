f84be87a241b5c332d4475b4cdd41e52dcaa93da
maftrew <jelmer_alphenaar@hotmail.com>
2015-03-05 11:26:33 +0000

    no prints

--- prob.py
+++ prob.py
@@ -7,16 +7,16 @@ class Prob():
         probDict = {}
         for key in ngram:
             biKey = key.rsplit(' ',1)[0]
-            print 'bikey is:'
-            print key
-            print biKey
+            #print 'bikey is:'
+            #print key
+            #print biKey
             numerator = ngram.get(key,None)
             if biKey in ngramMin1:
                 denominator = ngramMin1.get(biKey, None)
-            print numerator
-            print denominator
+            #print numerator
+            #print denominator
             prob = numerator/denominator
-            print prob
+            #print prob
             probDict.update({key: prob})
             
             
